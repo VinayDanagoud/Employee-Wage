670360a9b3e492437ff72ac32da8338a06f62e84
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-03-10 18:34:17 +0530

    Uc1 : Employee is Present or Not

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Employee Wage");
+Random random = new Random(); // using to generate the random numbers
+const int present = 1,
+          abcent = 0;
+
+int ispresent = random.Next(2); // 0,1
+if(ispresent == present)
+{
+    Console.WriteLine(" Employee is Present ");
+
+}
+else
+{
+    Console.WriteLine(" Employee is Abcent");
+}
