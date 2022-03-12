1526e3cb9a28a300b385b93875b633c1789f2a0b
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-03-12 17:28:32 +0530

    Uc2 : Calculated Emp wage

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -2,15 +2,22 @@
 Console.WriteLine("Employee Wage");
 Random random = new Random(); // using to generate the random numbers
 const int present = 1,
-          abcent = 0;
+             Abcent = 0,
+             perHrWage = 20,
+             fulltime = 8;
 
-int ispresent = random.Next(2); // 0,1
+
+
+int ispresent = random.Next(2), // 0,1
+    empWage = 0;
+
 if(ispresent == present)
 {
     Console.WriteLine(" Employee is Present ");
-
+    empWage = perHrWage * fulltime;
 }
 else
 {
     Console.WriteLine(" Employee is Abcent");
 }
+Console.WriteLine("Employee Wage : "+ empWage);
